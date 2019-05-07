251177f1db461faf6ad19833f144a65d1ac8e4fe
RAGAdox <rishitech882@gmail.com>
2019-05-07 09:49:18 +0530

    Issue Solved Using Promise and added Icons

--- src/components/services/loginService.js
+++ src/components/services/loginService.js
@@ -50,25 +50,17 @@ export default class LoginService extends React.Component {
         authUser.username = details.username;
         authUser.token = responseJson.token;
         this.setState({
-          //token:await SecureStore.getItemAsync('secure_token'),
           token: await AsyncStorage.getItem("secure_token"),
           success: responseJson.success,
           message: responseJson.message
         });
-        //console.warn(responseJson.name);
-        //await SecureStore.setItemAsync('secure_token',responseJson.token);
-        await AsyncStorage.setItem("secure_token", responseJson.token).then(
-          () => {
-            console.warn("token saved" + responseJson.token);
-            console.warn("Successed");
-            AsyncStorage.setItem("username", details.username).then(() => {
-            AsyncStorage.getItem("username", (err,result) => {
-                console.warn("Successed in saving and getting username"+result);
-              });
-              this.props.navigation.navigate("mainTabs");
-            });
-          }
-        );
+
+        let p1 = AsyncStorage.setItem("secure_token", responseJson.token);
+        let p2 = AsyncStorage.setItem("username", details.username);
+        Promise.all([p1, p2]).then(result => {
+          console.warn(" Saved to local Storage " + result);
+          this.props.navigation.navigate("home");
+        });
       } else {
         this.setState({
           token: "Invalid",
